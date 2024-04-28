6e5700d1a4f0839bae3aac574fc87363b1c9e8b7
Phil <phil.github@gmail.com>
2024-04-28 19:39:44 +0300

    Search Bar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,6 +6,26 @@ const Header = () => {
       <div class="heading">
         <h1 id="header">The Royal Bank of Flatiron</h1>
       </div>
+      {/*** Search Bar*/}
+      <div>
+        <div className="mt-2">
+          <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 ">
+            <input
+              type="text"
+              name="username"
+              id="username"
+              className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+              placeholder="Search Your Recent Transactions"
+            />
+            <button
+              type="submit"
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              🔍
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
