7f8e69bae7d470480094919db5f15a19aa96fba0
Phil <phil.github@gmail.com>
2024-04-28 19:43:21 +0300

    filter component

---
+++ src/Components/Filter.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Filter = () => {
+  return (
+    <>
+      {/*** Search Bar*/}
+
+      <div className="mt-2">
+        <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 ">
+          <input
+            type="text"
+            id="username"
+            className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+            placeholder="Search Your Recent Transactions"
+          />
+          <button
+            type="submit"
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            🔍
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Filter;
