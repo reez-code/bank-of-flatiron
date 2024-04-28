c76a95d3cabcb43e930bbc23ad285ad9f1c1bf3d
Phil <phil.github@gmail.com>
2024-04-28 20:44:59 +0300

    Table component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 
 import "./App.css";
 import Header from "./Components/Header";
-
+import Table from "./Components/Table";
 function App() {
   return (
     <>
       <Header />
+      <Table />
     </>
   );
 }
---
+++ src/Components/Table.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Table = () => {
+  return (
+    <>
+      <table class="table-auto">
+        <thead>
+          <tr>
+            <th>Song</th>
+            <th>Artist</th>
+            <th>Year</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default Table;
