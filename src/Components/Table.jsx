423f9ede10c338fe294f6af4de21add8b6eea9cc
Phil <phil.github@gmail.com>
2024-04-28 20:59:01 +0300

    table structure completed

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 
 const Table = () => {
+  const divStyle = {
+    borderWidth: "thick",
+  };
   return (
-    <>
-      <table class="table-auto">
-        <thead>
-          <tr>
-            <th>Song</th>
-            <th>Artist</th>
-            <th>Year</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-        </tbody>
-      </table>
-    </>
+    <table class="table-auto" id="table">
+      <thead style={divStyle}>
+        <tr>
+          <th style={divStyle}>Date</th>
+          <th style={divStyle}>Description</th>
+          <th style={divStyle}>Category</th>
+          <th style={divStyle}>Amount</th>
+        </tr>
+      </thead>
+      <tbody style={divStyle}>
+        <tr>
+          <td style={divStyle}>
+            The Sliding Mr. Bones (Next Stop, Pottersville)
+          </td>
+          <td style={divStyle}>Malcolm Lockyer</td>
+          <td style={divStyle}>1961</td>
+          <td style={divStyle}>random</td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
