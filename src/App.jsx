f0b85c4dcddd09dd6c1934bb5d19d20b0ce9b39e
Phil <phil.github@gmail.com>
2024-04-28 19:24:21 +0300

    Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
 
 import "./App.css";
+import Header from "./Components/Header";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div class="heading">
+        <h1 id="header">The Royal Bank of Flatiron</h1>
+      </div>
+    </>
+  );
+};
+
+export default Header;
