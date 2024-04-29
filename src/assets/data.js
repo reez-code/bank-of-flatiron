608144daf9227a0ad0d97be8a7ec635b712207ef
Phil <phil.github@gmail.com>
2024-04-29 16:23:37 +0300

    increased  data in data.js

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,10 +1,31 @@
 const transactions = [
   {
     id: 1,
-    date: "2020-01-06",
-    description: "Movies",
-    category: "Entertainment",
-    amount: 2000,
+    date: "2019-12-01",
+    description: "Paycheck from Bob's Burgers",
+    category: "Income",
+    amount: 1000,
+  },
+  {
+    id: 2,
+    date: "2019-12-01",
+    description: "KFC",
+    category: "Food",
+    amount: 250,
+  },
+  {
+    id: 3,
+    date: "2019-12-02",
+    description: "Burger King",
+    category: "Food",
+    amount: 800,
+  },
+  {
+    id: 4,
+    date: "2019-12-06",
+    description: "SunGlasses",
+    category: "Fashion",
+    amount: 800,
   },
 ];
 
